d4958a49e1760824af8abf632168e372c902bb58
Maxime <160537744+MaxHzd@users.noreply.github.com>
2024-05-31 21:12:09 +0200

    Add files via upload

--- scripts/recipes-data.js
+++ scripts/recipes-data.js
@@ -1,21 +1,18 @@
 const recipes = [
     {
         id: 1,
-        title: "Smoothie énergisant",
-        photo: "images/smoothie.jpg",
-        description: "Un smoothie riche en vitamines pour bien commencer la journée.",
-        ingredients: [
-            "1 banane",
-            "200ml de lait d'amande",
-            "1 cuillère à soupe de graines de chia",
-            "1 poignée de myrtilles",
-        ],
-        instructions: [
-            "Éplucher la banane et la couper en morceaux.",
-            "Mettre tous les ingrédients dans un blender.",
-            "Mixer jusqu'à obtenir une texture lisse.",
-            "Servir immédiatement.",
-        ]
+        title: "Courgettes farcies healthy",
+        photo: "images/Recettes/courgettes_healthy.jpg",
+        description: "Description de la recette des courgettes farcies.",
+        ingredients: ["2 Courgettes", "200g de poulet", "200g de sauce tomate", "1 oignon", "1 gousse d'ail", "parmesan", "origan", "épices chili", "sel, poivre"],
+        instructions: ["Préparer les courgettes...", "Cuire le poulet...", "Mélanger le tout...", "Farcir les courgettes...", "Cuire au four..."]
     },
-    // Ajouter d'autres recettes ici...
+    {
+        id: 2,
+        title: "Poulet sauce peanut butter",
+        photo: "images/Recettes/poulet_peanut_butter.jpg",
+        description: "Description de la recette du poulet sauce peanut butter.",
+        ingredients: ["200g de poulet", "200g de sauce tomate", "1 oignon", "1 gousse d'ail", "1 cas de peanut butter", "1 cas de jus de citron", "1 cas de sauce soja", "sel, poivre"],
+        instructions: ["Préparer les courgettes...", "Cuire le poulet...", "Mélanger le tout...", "Farcir les courgettes...", "Cuire au four..."]
+    },
 ];
